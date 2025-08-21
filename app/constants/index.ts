3c67ddb63e06e4b41c927bc67e760fbfc753aa0e
aditya-2k23 <holaaditya123@gmail.com>
2025-08-21 17:54:01 +0530

    Update icon paths in sidebar items for consistency

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -3,19 +3,19 @@ import type { AxisModel } from "@syncfusion/ej2-react-charts";
 export const sidebarItems = [
   {
     id: 1,
-    icon: "/assets/icons/home.svg",
+    icon: "/icons/home.svg",
     label: "Dashboard",
     href: "/dashboard",
   },
   {
     id: 3,
-    icon: "/assets/icons/users.svg",
+    icon: "/icons/users.svg",
     label: "All Users",
     href: "/all-users",
   },
   {
     id: 4,
-    icon: "/assets/icons/itinerary.svg",
+    icon: "/icons/itinerary.svg",
     label: "AI Trips",
     href: "/trips",
   },
